49e6db3d30f98ffa7d8391f25c8d86afdd8e7df6
Marta Plantykow <m.plantykow@gmail.com>
2023-08-08 20:54:33 +0200

    Introduce config file
    This commit reduces the list of input agrguments and
    introduces main configuration file.
    
    Signed-off-by: Marta Plantykow <m.plantykow@gmail.com>

---
+++ configureme.py
@@ -0,0 +1,23 @@
+### [General settings]
+# Debug level: 1 for basic, 2 for full logging
+debug_level = 1
+
+### [Genetic algorithm]
+# Initial population size
+gen_population_size = 8
+# Number of epochs
+gen_epochs = 8
+# Max value that is considered for Kp
+gen_max_kp = 5
+# Max value that is considered for Ki
+gen_max_ki = 5
+# Number of random parents added to each epoch
+gen_num_random = 2
+# Number of the best parents that are replicated to create a new generation
+gen_num_inherited = 5
+# Number of the best parents that are replicated to create a new generation
+gen_num_replicated = 4
+# Mutation coefficient
+gen_mutation_coef = 1
+# Number of elite chromosomes
+gen_elite_size = 1
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import subprocess #nosec
 import shlex
 import argparse
 import os
+import configureme as config
 from evaluate import Creature
 
 class Range():
@@ -29,28 +30,17 @@ adapterlist = os.listdir('/sys/class/net/')
 parser = argparse.ArgumentParser(description='Genetic algorithm for PID in PTP implementation')
 
 #List of arguments
-parser.add_argument("--pop_size", default=8, choices=range(1,9999), type=int, help="Initial population size", metavar="[1-9999]")
-parser.add_argument("--epochs", default=10, choices=range(1,9999), type=int, help="Number of epochs", metavar="[1-9999]")
-parser.add_argument("--max_kp", default=15, choices=Range(0.0,9999.0), type=float, help="Max value that is considered for Kp", metavar="[0.0-9999.0]")
-parser.add_argument("--max_ki", default=15, choices=Range(0.0,9999.0), type=float, help="Max value that is considered for Ki", metavar="[0.0-9999.0]")
-parser.add_argument("--num_random", default=2, choices=range(0,9999), type=int, help="Number of random parents added to each epoch", metavar="[0-9999]")
-parser.add_argument("--num_inherited", default=5, choices=range(0,9999), type=int, help="Number of the best parents that are crossed to create a new generation", metavar="[0-9999]")
-parser.add_argument("--num_replicated", default=4, choices=range(1,9999), type=int, help="Number of the best parents that are replicated to create a new generation", metavar="[1-9999]")
-parser.add_argument("--mutation_coef", default=1, choices=range(0,9999), type=float, help="Mutation coefficient", metavar="[0-9999]")
-parser.add_argument("--debug_level", default=1, choices=range(1,2), type=int, help="Debug level: 1 for basic; 2 for full logging", metavar="[1-2]")
 parser.add_argument("--i", type=str, choices = adapterlist, help="Interface")
 parser.add_argument("--t", default=120, choices=range(1,9999), type=int, help="-t from PTP script", metavar="[1-9999]")
 parser.add_argument("--metric", default=1, choices=range(1,3), type=int, help="Metric type: 1 for MSE; 2 for RMSE; 3 for MAE", metavar="[1-3]")
-parser.add_argument("--elite_size", default=1, choices=range(1,9999), type=int, help="Number of elite chromosoms", metavar="[1-9999]")
 
 args = parser.parse_args()
 
 #Debug levels
-if args.debug_level == 1:
+if config.debug_level == 1:
     debug_l1 = 1
     debug_l2 = 0
-
-if args.debug_level == 2:
+else:
     debug_l1 = 1
     debug_l2 = 1
 
@@ -64,8 +54,8 @@ elite = []
 if debug_l1:
     print("Creating initial population...")
 
-for _ in range(args.pop_size):
-    population.append(Creature(random.uniform(0, args.max_kp), random.uniform(0, args.max_ki))) #nosec
+for _ in range(config.gen_population_size):
+    population.append(Creature(random.uniform(0, config.gen_max_kp), random.uniform(0, config.gen_max_ki))) #nosec
 
 if debug_l1:
     iter = 0
@@ -78,7 +68,7 @@ if debug_l1:
             print(creature.Ki)
             iter = iter + 1
 
-for epoch in range(args.epochs):
+for epoch in range(config.gen_epochs):
     if debug_l1:
         print("***************************************************************")
         print("EPOCH NUMBER ", epoch)
@@ -95,7 +85,7 @@ for epoch in range(args.epochs):
         new_kp = round(parent.Kp,2)
         new_ki = round(parent.Ki,2)
         parent.mutate(new_kp, new_ki)
-        parent.evaluate_data(args.debug_level, args.i, args.t, args.metric)
+        parent.evaluate_data(config.debug_level, args.i, args.t, args.metric)
         score.append(parent.rating)
         i = i + 1
 
@@ -116,13 +106,13 @@ for epoch in range(args.epochs):
         print("Sorted Scores indexes: ", sorted_scores_indexes)
 
     #Create Elite
-    for i in range(0,args.elite_size):
+    for i in range(0,config.gen_elite_size):
         index = sorted_scores_indexes[i]
         elite.append(population[index])
     elite.sort(key = lambda Creature: Creature.rating)
 
     for i in range(0, len(elite)):
-        if i == args.elite_size:
+        if i == cinfig.gen_elite_size:
             del elite[i:len(elite)]
             break
 
@@ -133,9 +123,9 @@ for epoch in range(args.epochs):
     if debug_l1:
         print("Crossing parents...")
         x = 0
-    for x in range(args.num_inherited):
+    for x in range(confg.gen_num_inherited):
         y = x + 1
-        for _ in range(args.num_inherited - x - 1):
+        for _ in range(confug.gen_num_inherited - x - 1):
             if debug_l1:
                 print(x, " + ", y)
             new_generation.append(
@@ -159,7 +149,7 @@ for epoch in range(args.epochs):
     #Replicating parents
     if debug_l1:
         print("Replicating parents...")
-    for x in range(args.num_replicated):
+    for x in range(config.gen_num_replicated):
         new_generation.append(Creature(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[x]].Ki))
     if debug_l1:
         print("New generation creation - replicated creatures added!")
@@ -178,8 +168,8 @@ for epoch in range(args.epochs):
     #Adding randoms
     if debug_l1:
         print("Adding new random parents")
-    for _ in range(args.num_random):
-        new_generation.append(Creature(random.uniform(0, args.max_kp), random.uniform(0, args.max_ki))) #nosec
+    for _ in range(config.gen_num_random):
+        new_generation.append(Creature(random.uniform(0, config.gen_max_kp), random.uniform(0, config.gen_max_ki))) #nosec
     if debug_l1:
         print("New generation creation - random creatures added!")
     if debug_l2:
@@ -198,11 +188,11 @@ for epoch in range(args.epochs):
         print("Mutants are coming...")
     for creature in new_generation:
         rand_x = random.uniform(-1, 1) #nosec
-        new_kp = creature.Kp + (rand_x * args.mutation_coef)
-        new_kp = max(0, min(new_kp, args.max_kp))
+        new_kp = creature.Kp + (rand_x * config.gen_mutation_coef)
+        new_kp = max(0, min(new_kp, config.gen_max_kp))
         rand_y = random.uniform(-1, 1) #nosec
-        new_ki = creature.Ki + (rand_y * args.mutation_coef)
-        new_ki = max(0, min(new_ki, args.max_ki))
+        new_ki = creature.Ki + (rand_y * config.gen_mutation_coef)
+        new_ki = max(0, min(new_ki, config.gen_max_ki))
         creature.mutate(new_kp, new_ki)
     if debug_l1:
         print("Mutation finished!")
@@ -217,7 +207,7 @@ for epoch in range(args.epochs):
     #Print information about progress
     number_of_creatures = len(new_generation)
     progress = number_of_creatures * (epoch + 1)
-    epoch_progress = number_of_creatures * args.epochs
+    epoch_progress = number_of_creatures * config.gen_epochs
     print("***************************************************************")
     print("Progress: ", progress/epoch_progress * 100, "%")
     print("***************************************************************")
@@ -231,7 +221,7 @@ with open("log.log", "a") as f:
         f.write(f"{population[index].Kp};{population[index].Ki};{score[index]}\n")
 
 default = Creature(0.7,0.3)
-default.evaluate_data(args.debug_level, args.i, args.t, args.metric)
+default.evaluate_data(debug_level, args.i, args.t, args.metric)
 
 with open("log.log", "a") as f:
     f.write("\nDefault setings:\n")
